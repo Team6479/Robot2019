1ee0ee170988a69a54dd09cb01d7bd7b0d5a936e
iboyperson <tjquillan@gmail.com>
2019-01-09 15:24:28 -0700

    Fix ShuffleboardTabs
    Unklusted it!

---
+++ src/main/java/frc/robot/util/ShuffleboardTabs.java
@@ -0,0 +1,11 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class ShuffleboardTabs {
+    public static ShuffleboardTab drive = Shuffleboard.getTab("Drive");
+    public static ShuffleboardTab config = Shuffleboard.getTab("Config");
+    public static ShuffleboardTab diagnostics = Shuffleboard.getTab("Diagnostics");
+
+}
